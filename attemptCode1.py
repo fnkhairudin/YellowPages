2a6460d9742eefde9519e20c787f5ca4ea4f9c2e
Faisal <fnkhairudin@gmail.com>
2023-05-26 18:10:03 +0700

    troubleshooting delete & add menu

--- attemptCode1.py
+++ attemptCode1.py
@@ -3,13 +3,17 @@ import os
 import sys
 import pyinputplus as pyip
 import tabulate
-## fungsi sorting based on ID, companyName, businessField, or city could be added in readMenu --> diubah dulu ke dictionary datanya
-## select sub-menu in each Menu still doesn't work properly (looping doesnt work properly, especially addMenu)
-## input validation, double check!!
-## else in sub-menu doesnt work properly
-## indexing start from 1 ??
-## Read menu option 1 not yet added
-## data and choices should be 
+# fungsi sorting based on ID, companyName, businessField, or city could be added in readMenu --> diubah dulu ke dictionary datanya
+# select sub-menu in each Menu still doesn't work properly (looping doesnt work properly, especially addMenu, deleteMenu)
+# input validation, double check!!
+# indexing start from 1 ?
+# shall we save variables "data" and "choices" in other function ?
+# dictionary data type can use in tabulate format also
+# DELETE MENU : pay attention if user want to change a column that not str data type
+# phoneNumber : provide country phone code ?
+# choices dont use len(data) [DONE]
+# show latest database after add or delete [DONE]
+# Read menu option 1 not yet added [DONE]
 
 path = r'C:\Users\faisa\Desktop\DataSciencePurwadhika\Modul1\CapstoneProjectModul1\dbYellowPages.csv'
 
@@ -63,12 +67,13 @@ def mainMenu(): ## input validation ##
             if userInput == 1:
                 readMenu(db)
             elif userInput == 2:
-                addMenu(db)
+                # addMenu(db)
                 db = addMenu(db) # return latest db
+                continue
             elif userInput == 3:
                 updateMenu()
             elif userInput == 4:
-                deleteMenu(db)
+                # deleteMenu(db)
                 db = deleteMenu(db) # return latest db
             # eval(userInput)
         # Otherwise, exit from the menu
@@ -79,7 +84,7 @@ def mainMenu(): ## input validation ##
     # Open database in write condition
     file = open(path, 'w')
 
-    # Keep the database update
+    # Keep the database up to date
     writer = csv.writer(file, lineterminator='\n', delimiter=';')
     columns = list(db.values())[0] # termasuk kolom dan data
     data = list(db.values())[1:]
@@ -119,16 +124,15 @@ def readMenu(database):
                 print('\n')
         # If user choose 2nd option
         elif userInput == 'Show database in detail':
-            choices1 = [id for id in range(len(data))]
+            choices1 = [data[index][0] for index in range(len(data))]
             userInput1 = int(input("Which ID do you want to return ?\n"))
             #userInput1 = pyip.inputInt(prompt='Which ID do you want to return ?\n', blockRegexes=[r'a-zA-Z'], lessThan=len(data))
             # if ID (index) doesn't exist in database
             if userInput1 not in choices1:
                 print('Data does not exist!\n')
-                readMenu(database)
-            # if ID (index) exist in database
+            # else: ID (index) exist in database
             else:
-                print(tabulate.tabulate(data[userInput1:userInput1+1], headers=columns, tablefmt="github"))
+                print(tabulate.tabulate(list([database[str(userInput1)]]), headers=columns, tablefmt="github"))
                 print('\n')
 
         # back to main menu ('Kembali ke Main Menu')
@@ -146,6 +150,8 @@ def addMenu(database):
     Returns:
         database: latest database
     """
+    # flag = True
+
     # list of data
     data = list(database.values())[1:]
 
@@ -154,7 +160,7 @@ def addMenu(database):
         userInput = pyip.inputMenu(prompt='Select Add Menu:\n', choices=choices, numbered=True)
         if userInput == 'Menambahkan data Yellow Pages':
         # check the ID does exist or not ?
-            choices = [id for id in range(len(data))]
+            choices = [data[index][0] for index in range(len(data))]
             userInputIndex = pyip.inputInt(prompt='Masukkan ID (index) yang ingin ditambahkan: ') # input ID
             # if data already exist, show notification 'Data already exist!'
             if userInputIndex in choices:
@@ -168,23 +174,27 @@ def addMenu(database):
                 email = pyip.inputEmail(prompt='input email: ')
                 
                 # print added data in tabular format
-                tabularAddedData = [[userInputIndex, companyName, businessField, city, phoneNumber, email]]
-                print(tabulate.tabulate(tabularAddedData, headers=columns, tablefmt="github"))
+                tabularAddedData = [userInputIndex, companyName, businessField, city, phoneNumber, email]
+                print(tabulate.tabulate(list([tabularAddedData]), headers=columns, tablefmt="github"))
 
                 ## saving menu option
                 savingMenuInput = pyip.inputYesNo(prompt='Are you sure want to save the data ? (Yes/No):')
                 if savingMenuInput == 'yes':
                     database.update(
-                        {f'{userInputIndex}': [len(database)-1, companyName, businessField, city, phoneNumber, email]})
+                        {f'{userInputIndex}': [userInputIndex, companyName, businessField, city, phoneNumber, email]})
                     
                     # show data after added data in database
-                    readMenu(database)
+                    data.append(tabularAddedData)
+                    print(tabulate.tabulate(data, headers=columns, tablefmt="github"))
+                    # readMenu(database)
                     print('\nData successfully saved!')
                 else:
                     print('\nOkey double check your input data!')
         else: # Still doesnt work properly
-            #print('doesnt work properly')
+            # print('doesnt work properly')
+            # flag = False
             break
+            
     # return latest database after added new data
     return database
 
@@ -203,7 +213,7 @@ def deleteMenu(database):
     data = list(database.values())[1:]
 
     # available ID
-    choices = [id for id in range(len(data))]
+    choices = [data[index][0] for index in range(len(data))]
 
     # select delete menu
     while True:
@@ -213,7 +223,7 @@ def deleteMenu(database):
             userInput = pyip.inputInt(prompt='Enter ID that you want to delete in database:')
             if userInput in choices:
                 # print data that you want to delete in tabular format
-                print(tabulate.tabulate(data[userInput:userInput+1], headers=columns, tablefmt="github"))
+                print(tabulate.tabulate(list([database[str(userInput)]]), headers=columns, tablefmt="github"))
                 
                 # Ensure user whether to delete or not ?
                 deletingMenuInput = pyip.inputYesNo(prompt='Are you sure want to delete the data ? (Yes/No):')
@@ -221,7 +231,7 @@ def deleteMenu(database):
                 if deletingMenuInput == 'yes':
                     del database[str(userInput)]
                     # show database after data is deleted
-                    readMenu(database)
+                    print(tabulate.tabulate(list(database.values())[1:], headers=columns, tablefmt="github"))
                     print('\nData successfully deleted!')
                 else:
                     print('Okey double check your input!\n')
@@ -233,19 +243,40 @@ def deleteMenu(database):
 
 # update data
 def updateMenu(database):
-    #print('updateMenu In Progress')
-    # list of data
-    data = list(database.values())[1:]
+    print('updateMenu In Progress')
+    # # list of data
+    # data = list(database.values())[1:]
 
-    # available ID
-    choices = [id for id in range(len(data))]
+    # # available ID
+    # choices = [id for id in range(len(data))]
 
-    # select update menu
-    while True:
-        choices1 = ['Update data in Yellow Pages database', 'Back to Main Menu']
-        userInput = pyip.inputMenu(prompt='Select Update Menu:\n', choices=choices1, numbered=True)
-        if userInput == 'Update data in Yellow Pages database':
-            userInputIndex = pyip.inputInt(prompt='Enter ID that you want to update in database:')
+    # # select update menu
+    # while True:
+    #     choices1 = ['Update data in Yellow Pages database', 'Back to Main Menu']
+    #     userInput = pyip.inputMenu(prompt='Select Update Menu:\n', choices=choices1, numbered=True)
+    #     if userInput == 'Update data in Yellow Pages database':
+    #         userInputIndex = pyip.inputInt(prompt='Enter ID that you want to update in database:')
+    #         if userInputIndex not in choices:
+    #             print("Data doens't exist!")
+    #         else:
+    #             print(tabulate.tabulate(data[userInputIndex:userInputIndex+1], headers=columns, tablefmt="github"))
+    #             inputContinueUpdate = pyip.inputYesNo(prompt='Do you want continue to update ? (yes/no): ')
+    #             if inputContinueUpdate == 'no':
+    #                 continue
+    #             else:
+    #                 columns = list(db.values())[0] 
+    #                 # ['ID', 'companyName', 'businessField', 'City', 'phoneNumber', 'Email']
+
+    #                 inputUserColumn = pyip.inputMenu(prompt='Which column do you want to update ?', choices=columns, numbered=True) # output string
+    #                 if inputUserColumn == 'ID':
+                    
+    #                 elif inputUserColumn == 'companyName':
+
+    #                 elif inputUserColumn == 'businessField':
+                    
+    #                 elif inputUserColumn == 'city':
+                    
+    #                 elif
 
 
 mainMenu()
